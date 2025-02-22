8b32efa219ed9cadd7737608d97291ec71c12bff
Pawanjot Singh <pawanjotsingh@192-168-1-100.tpgi.com.au>
2025-02-22 22:54:07 +1100

    Updted logic to get HostName

--- src/electron/peer/peerHandler.ts
+++ src/electron/peer/peerHandler.ts
@@ -2,8 +2,8 @@ import { Socket } from "net"
 import { PeerComms } from "../constants/PeerComms.js";
 import Device from "../types/Device.js";
 import { ip } from "address";
-import { hostname } from 'os';
 import { AppSettings } from "../constants/AppSettings.js";
+import { getDeviceType, getHostName } from "../utils.js";
 
 export const peerHandler = (soc: Socket) => {
     soc.on('data', (data) => {
@@ -14,8 +14,8 @@ export const peerHandler = (soc: Socket) => {
                  
                 const device: Device = {
                     ip: ip()!,
-                    name: hostname(),
-                    type: 'device'
+                    name: getHostName(),
+                    type: getDeviceType()
                 }
                 soc.write(PeerComms.HELLO + AppSettings.DELIMITER_STRING + JSON.stringify(device));
                 break;
--- src/electron/utils.ts
+++ src/electron/utils.ts
@@ -1,7 +1,41 @@
+import cp from "child_process";
+import os from 'os';
+
 export function isDev(): boolean {
     return process.env.NODE_ENV === 'dev';
 }
 
 export function getSubnet(ip: string): string {
     return ip.slice(0, ip.lastIndexOf('.'));
+}
+
+export function getHostName(): string {
+    const platform = os.platform();
+
+    switch(platform) {
+        case "win32":
+            return process.env.COMPUTERNAME!;
+        case "darwin":
+            return cp.execSync("scutil --get ComputerName").toString().trim();
+        case "linux":
+            const name = cp.execSync("hostnamectl --pretty").toString().trim();
+            return name === "" ? os.hostname() : name;
+        default:
+            return os.hostname();
+    }
+}
+
+export function getDeviceType(): string {
+    const platform = os.platform();
+
+    switch(platform) {
+        case "win32":
+            return "Windows";
+        case "darwin":
+            return "Mac OS";
+        case "linux":
+            return "linux";
+        default:
+            return "Unknown Device";
+    }
 }
